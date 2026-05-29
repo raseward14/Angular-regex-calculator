4ed4be53b61c4055d2a856ac549459386f8a6dcf
raseward14 <raseward14@gmail.com>
2026-05-29 11:31:34 -0600

    added an observable and reactive form for the regex input and the textarea for the test string - added rxjs debounceTime to only test the regex after the user has stopped editing the regex for longer than a half second

--- src/app/regex/regex.component.ts
+++ src/app/regex/regex.component.ts
@@ -1,13 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-regex',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './regex.component.html',
   styleUrl: './regex.component.css',
 })
 export class RegexComponent {
-  regex = '';
+  regexForm = new FormGroup({
+    regexControl: new FormControl('')
+  }
+  )
   testString = 'this is a hard coded test string';
+
+  private destroyRef = inject(DestroyRef);
+  
+  constructor() {
+    const subscription = this.regexForm.valueChanges
+      .pipe(
+        debounceTime(500), // Wait for 500ms of inactivity
+        distinctUntilChanged(), // Only emit if the value has changed
+        tap(value => console.log('regex changed:', value))
+      ).subscribe();
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
 }
