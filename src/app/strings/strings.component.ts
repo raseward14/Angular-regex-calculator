d77f67eff6957f4322bbdfe54cec56f84800a0fe
raseward14 <raseward14@gmail.com>
2026-06-16 17:47:05 -0600

    added the stringMethod type and method array with a calculated operation signal and a user input signal. basic display table styles applied too and input radio so only one string method can be selected at a time

--- src/app/strings/strings.component.ts
+++ src/app/strings/strings.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import type { StringMethod } from '../types';
 
 @Component({
   selector: 'app-strings',
+  imports: [FormsModule],
   standalone: true,
   templateUrl: './strings.component.html',
   styleUrl: './strings.component.css',
 })
-export class StringsComponent {}
+export class StringsComponent {
+  userInput = signal('');
+  calculatedOperation = signal('');
+  stringMethods = [
+    { method: 'length', description: 'property returns the length of a string' },
+    {
+      method: 'at(position)',
+      description:
+        'method returns the character at a specific index (position) in a string. Allows negative indexes.',
+    },
+    {
+      method: 'chatAt(position)',
+      description:
+        'method returns the character at a specific index (position) in a string. Does not allow negative indexes.',
+    },
+  ];
+
+  calculatedResult = computed(() => {
+    return this.userInput().length;
+  });
+
+  onMethodChange(method: StringMethod) {
+    console.log(method.method);
+    if (method.method !== this.calculatedOperation()) {
+      this.calculatedOperation.set(method.method);
+    } else {
+      this.calculatedOperation.set('');
+    }
+    console.log(this.calculatedOperation());
+  }
+}
--- src/app/types.ts
+++ src/app/types.ts
@@ -2,3 +2,8 @@ export type Flag = {
   name: string;
   description: string;
 };
+
+export type StringMethod = {
+  method: string;
+  description: string;
+};
